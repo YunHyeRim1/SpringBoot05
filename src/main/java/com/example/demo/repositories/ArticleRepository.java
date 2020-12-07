ab71c47a9ae231122f8c932e7f0f703905f6d5cd
YunHyeRim1 <0305_08@naver.com>
2020-12-07 10:14:09 +0900

    2020-12-07 10:00

---
+++ src/main/java/com/example/demo/repositories/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domains.ArticleDto;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository {
+   
+}
