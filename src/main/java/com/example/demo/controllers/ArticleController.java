569f2a1425d325e6dd851672391cf3b9c10341bc
YunHyeRim1 <0305_08@naver.com>
2020-12-07 14:59:51 +0900

    14

--- src/main/java/com/example/demo/controllers/ArticleController.java
+++ src/main/java/com/example/demo/controllers/ArticleController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
+import com.example.demo.utils.Printer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArticleController{
-   
+   @Autowired Printer printer;
 }
